f2223c28ac8749b2b11a639ceac41b6ab44198f8
Ro706 <60247178+Ro706@users.noreply.github.com>
2020-05-17 16:51:18 +0000

    update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,26 @@
-GNU nano 4.6               1.py
-def hello():
-    print "hello,today's topic is gravity:-"
-hello()
-print "Gravity is the force of attraction between any two>
-
-def gravity():
-    return (G * m1 * m2 / r)
-G=input("G=")
-m1=input("m1=")
-m2=input("m2=")
-r=input("r=")
-print "f=",gravity()
+# converts temperature to fahrenheit or celsius
+ 
+def print_options():
+    print "Options:"
+    print " 'p' print options"
+    print " 'c' convert from celsius"
+    print " 'f' convert from fahrenheit"
+    print " 'q' quit the program"
+ 
+def celsius_to_fahrenheit(c_temp):
+    return 9.0 / 5.0 * c_temp + 32
+ 
+def fahrenheit_to_celsius(f_temp):
+    return (f_temp - 32.0) * 5.0 / 9.0
+ 
+choice = "p"
+while choice != "q":
+    if choice == "c":
+        temp = input("Celsius temperature: ")
+        print "Fahrenheit:", celsius_to_fahrenheit(temp)
+    elif choice == "f":
+        temp = input("Fahrenheit temperature: ")
+        print "Celsius:", fahrenheit_to_celsius(temp)
+    elif choice != "q":
+        print_options()
+    choice = raw_input("option: ")
