f77bc5fba727d1cba09831cf47cad9ee361e3594
Ro706 <60247178+Ro706@users.noreply.github.com>
2020-05-17 13:55:45 +0000

    Create 1.py
    By this u can calculate force of gravity..

---
+++ 1.py
@@ -0,0 +1,13 @@
+GNU nano 4.6               1.py
+def hello():
+    print "hello,today's topic is gravity:-"
+hello()
+print "Gravity is the force of attraction between any two>
+
+def gravity():
+    return (G * m1 * m2 / r)
+G=input("G=")
+m1=input("m1=")
+m2=input("m2=")
+r=input("r=")
+print "f=",gravity()
